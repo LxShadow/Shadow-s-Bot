29253c44dfba77d816381d8efb78b1d561b7461d
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-19 23:22:25 -0600

    𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Mystic - Bot\nWhatsApp Bot MD', {
+say('Shadows\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot Created By Shadow`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
